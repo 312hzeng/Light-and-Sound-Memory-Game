ced3602894f8c3d84cf14b4d6897a4a45ff30510
Glitch (glitch-hello-website) <none>
2022-03-10 22:39:11 +0000

    🚋🎻 Checkpoint ./script.js:42993051/1340 ./index.html:42993051/333

--- script.js
+++ script.js
@@ -3,19 +3,28 @@
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
 
-var tonePlaying = false;
-var volume = 0.5;  
+//global constant
+const clueHoldTime = 1000; //how long to hold each clue's light/sound
+const cluePauseTime = 333; //how long to pause in between clues
+const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
+//global var
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0; 
 var gamePlaying = false;
+var guessCounter = 0;
 
+var tonePlaying = false;
+var volume = 0.5;  
+
+//start and end game func
 function startGame(){
     //initialize game variables
     progress = 0;
     gamePlaying = true;
     document.getElementById("startBtn").classList.add("hidden");
     document.getElementById("stopBtn").classList.remove("hidden");
+    playClueSequence();
 }
 function stopGame(){
     //initialize game variables
@@ -65,9 +74,46 @@ g.gain.setValueAtTime(0,context.currentTime)
 o.connect(g)
 o.start(0)
 
+function playSingleClue(btn){
+  if(gamePlaying){
+    lightButton(btn);
+    playTone(btn,clueHoldTime);
+    setTimeout(clearButton,clueHoldTime,btn);
+  }
+}
+
+function playClueSequence(){
+  guessCounter = 0;
+  let delay = nextClueWaitTime; //set delay to initial wait time
+  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
+    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
+    delay += clueHoldTime 
+    delay += cluePauseTime;
+  }
+}
+
 function lightButton(btn){
   document.getElementById("button"+btn).classList.add("lit")
 }
 function clearButton(btn){
   document.getElementById("button"+btn).classList.remove("lit")
+}
+
+//user response
+function loseGame(){
+  stopGame();
+  alert("Game Over. You lost.");
+}
+function Gamwine(){
+  stopGame();
+  alert("Game Over. You won!");
+}
+function guess(btn){
+  console.log("user guessed: " + btn);
+  if(!gamePlaying){
+    return;
+  }
+  
+  // add game logic here
 }
